c0be04e62c5db7a0fcaca55120bbba355bdfae51
muszuz <ann_s@onet.pl>
2017-02-07 15:01:36 +0100

    Update pythia8.C

--- pythia8.C
+++ pythia8.C
@@ -146,7 +146,7 @@
       Double_t norm = Cp->GetEntries();
       Cp->Scale(1/norm);
       Cp->Draw();
-      Cp->GetXaxis()->( std::string(1, (char)232) + " [rad]");
+      Cp->GetXaxis()->("theta [rad]");
       //Cp->GetYaxis()->("dΓ/Γ");
 
     }
